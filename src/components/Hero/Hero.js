d1fc6417d63e253bee163bd453682882f3d5e0e0
Sultana Tasnim Jahan <tasnim047sultana@gmail.com>
2021-06-11 00:21:18 +0600

    fixed all href attribute

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -37,7 +37,7 @@ export default function Hero() {
                   >
                     <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                       <div className="flex items-center justify-between w-full md:w-auto">
-                        <a href="">
+                        <a href="/home">
                           <span className="sr-only">Workflow</span>
                           <img
                             className="h-8 w-auto sm:h-10"
@@ -109,7 +109,7 @@ export default function Hero() {
                         ))}
                       </div>
                       <a
-                        href=""
+                        href="/login"
                         className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
                       >
                         Log in
@@ -134,7 +134,7 @@ export default function Hero() {
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <a
-                    href=""
+                    href="/home"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                   >
                     Get started
@@ -142,7 +142,7 @@ export default function Hero() {
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
                   <a
-                    href=""
+                    href="/home"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                   >
                     Live demo
